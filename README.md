934352c173910ceb984f4905757f1807856de259
Youssef <yussdev@gmail.com>
2023-07-12 18:06:30 +0330

    feat(gh-pages): add build folder

--- README.md
+++
@@ -1,3 +0,0 @@
-# Augmented Token Bonding Curve Design
-
-Experiment and test augmented token bonding curves, part of the Commons Stack
---
+++ precache-manifest.4578d5eefebfeb8c74369e55044c9e79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa02baa347d001136e09013bcf3ca48",
+    "url": "/augmented-tbc-design-v2/index.html"
+  },
+  {
+    "revision": "4811ee0f1859d76e5a87",
+    "url": "/augmented-tbc-design-v2/static/css/main.68a8f8f6.chunk.css"
+  },
+  {
+    "revision": "7bdb3b84f39b9a5e12a4",
+    "url": "/augmented-tbc-design-v2/static/js/2.a0758485.chunk.js"
+  },
+  {
+    "revision": "4811ee0f1859d76e5a87",
+    "url": "/augmented-tbc-design-v2/static/js/main.b245ebe8.chunk.js"
+  },
+  {
+    "revision": "4ff8a4385018e0d2b9d5",
+    "url": "/augmented-tbc-design-v2/static/js/runtime~main.744a1519.js"
+  }
+]);
